93265eb81b0fe441dbc14642a882a2697b9b8ff8
MayronSouza <mayron.souza@gmz.solutions>
2024-05-30 13:59:50 -0300

    feat: implement transaction create route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,12 @@ import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 
 export const transactionsRoutes = async (app: FastifyInstance) => {
+  app.get('/', async () => {
+    const transaction = await connection('transactions').select()
+
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
